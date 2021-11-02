bca36ac197c1267e98769fc073d1b0d594d46da6
Mahudoom <66937573+easa2000@users.noreply.github.com>
2021-11-02 23:54:39 +0530

    Update Double Linkedlist.c

--- Double Linkedlist.c
+++ Double Linkedlist.c
@@ -6,3 +6,23 @@ struct node{
     int data;
     struct node *next, *prev;
 }*n,*head, *tail, *t;
+
+// inside at beginning
+
+void ins_at_beg_dll(int num) {
+    n = (struct node*)malloc(sizeof(struct node));
+    n->data  = num;
+    n->next  = NULL;
+    n->prev = NULL;
+
+    if(head == NULL) {
+        head = n;
+        tail = n;
+    }
+    else
+    {
+        n->next = head;
+        head->prev = n;
+        head = n;
+    }
+}
